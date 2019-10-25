f0ec5a24bbe25f619382b348c10f696ddb2b76b5
Lam <nguyen.h.lam12@gmail.com>
2019-10-25 13:20:04 -0500

    populating data

--- LearningReact/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
+++ LearningReact/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
@@ -1,19 +1,78 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// var style = {
+//     backgroundColor: 'orange',
+//     color: 'white',
+//     fontFamily: 'Arial'
+// }
+
+
+
+// ReactDOM.render(
+//     <div style={style}>
+//         <h1 id="heading-element">Hello World!</h1>
+//         <p>We're glad you're here</p>
+//     </div>,
+//     document.getElementById('root')
+// )
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// class Message extends React.Component {
+//     render() {
+//         console.log()
+//         return (
+//             <div>
+//                 <h1 style={{ color: this.props.color }}>{this.props.msg}
+//                 </h1>
+//                 <p>I'll check back in {this.props.minutes} minutes</p>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<Message color="blue" msg="how are you?" minutes={5} />, document.getElementById('root')
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Arial'
+let skiData = {
+    total: 50,
+    powder: 20,
+    backcountry: 10,
+    goal: 100
 }
 
-const title = React.createElement(
-    'h1',
-    { id: 'title', className: 'header', style: style },
-    'Hello World'
-)
+class SkiDayCounter extends React.Component {
+    render() {
+        return (
+            <section>
+                <div>
+                    <p>Total Days: {this.props.total}</p>
+                </div>
+                <div>
+                    <p>Powder Days: {this.props.powderK}</p>
+                </div>
+                <div>
+                    <p>Backcountry Days: {this.props.backcountry}</p>
+                </div>
+                <div>
+                    <p>Goal: {this.props.goal}</p>
+                </div>
+            </section>
+        )
+    }
+}
 
 ReactDOM.render(
-    title,
+    <SkiDayCounter
+        total={skiData.total}
+        powder={skiData.powder}
+        backgcountry={skiData.backcountry}
+        goal={skiData.goal}
+    />,
     document.getElementById('root')
 )
