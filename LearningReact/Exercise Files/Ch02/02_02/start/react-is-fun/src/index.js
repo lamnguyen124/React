6f9ffd89c1fb3a93b68e8fa089adbc39c7cbd3cc
Lam <nguyen.h.lam12@gmail.com>
2019-10-25 16:36:31 -0500

    finished with exercise

--- LearningReact/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
+++ LearningReact/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
@@ -113,28 +113,29 @@
 
 import React from 'react'
 import { render } from 'react-dom'
+import PropTypes from 'prop-types'
 
 let bookList = [
-    { "title": "Hunger", "author": "Roxane Gay", "pages": 320 },
+    { "title": "Hunger" },
     { "title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
     { "title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
     { "title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 }
 ]
 
-const Book = ({ title, author, pages, freeBookmark }) => {
+const Book = ({ title = "No Title Provided", author = "No Author", pages = 0, freeBookmark }) => {
     return (
         <section>
             <h2>{title}</h2>
             <p>by: {author}</p>
             <p>Pages: {pages} pages</p>
-            <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'} </p>
+            <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
         </section>
     )
 }
 
 const Hiring = () =>
     <div>
-        <p>The library is hiring. go to www.library.com/jobs for more</p>
+        <p>The library is hiring. Go to www.library.com/jobs for more.</p>
     </div>
 
 const NotHiring = () =>
@@ -144,10 +145,29 @@ const NotHiring = () =>
 
 class Library extends React.Component {
 
+    static defaultProps = {
+        books: [
+            { "title": "Tahoe Tales", "author": "Chef Whitley", "pages": 1000 }
+        ]
+    }
+
     state = {
         open: true,
         freeBookmark: false,
-        hiring: true
+        hiring: true,
+        data: [],
+        loading: false
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true })
+        fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+            .then(data => data.json())
+            .then(data => this.setState({ data, loading: false }))
+    }
+
+    componentDidUpdate() {
+        console.log("The component just updated")
     }
 
     toggleOpenClosed = () => {
@@ -160,6 +180,21 @@ class Library extends React.Component {
         return (
             <div>
                 {this.state.hiring ? <Hiring /> : <NotHiring />}
+                {this.state.loading
+                    ? "loading..."
+                    : <div>
+                        {this.state.data.map(product => {
+                            return (
+                                <div key={product.id}>
+                                    <h3>Library Product of the Week!</h3>
+                                    <h4>{product.name}</h4>
+                                    <img alt={product.name} src={product.image} height={100} />
+                                </div>
+                            )
+                        })}
+
+                    </div>
+                }
                 <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
                 <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
@@ -176,9 +211,42 @@ class Library extends React.Component {
     }
 }
 
+// class FavoriteColorForm extends React.Component {
+//     slate = { value: '' }
+//     newColor = e =>
+//         this.setState({ value: e.target.value })
 
+//     submit = e => {
+//         console.log("New Color: ${this.state.value}")
+//         e.preventDefault()
+//     }
+
+//     render() {
+//         return (
+//             <form onSubmit={this.submit}>
+//                 <label>Favorite Color:
+//                 <input type="color"
+//                         onChange={this.newColor} />
+//                 </label>
+//                 <button>Submit</button>
+//             </form>
+//         )
+//     }
+// }
+
+Library.propTypes = {
+    books: PropTypes.array
+}
+
+Book.propTypes = {
+    title: PropTypes.string,
+    author: PropTypes.string,
+    pages: PropTypes.number,
+    freeBookmark: PropTypes.bool
+}
 
 render(
+    // <FavoriteColorForm />,
     <Library books={bookList} />,
     document.getElementById('root')
 )
