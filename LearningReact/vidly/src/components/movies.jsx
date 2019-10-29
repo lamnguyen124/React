9a496a923aaf7c8182394779cd77d756bf04d1c8
Lam <nguyen.h.lam12@gmail.com>
2019-10-29 15:51:33 -0500

    listed movies with delete function

--- LearningReact/vidly/src/components/movies.jsx
+++ LearningReact/vidly/src/components/movies.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
 
 class Movies extends Component {
-    state = {  }
+    state = {
+        movies: getMovies()
+    };
+
+    handleDelete = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({ movies});
+    };
+
     render() { 
-        return (<h2>Movies Component</h2>);
+        return <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.movies.map(movie => (
+                <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                        <button 
+                            onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete
+                        </button>
+                    </td>
+                </tr>
+                ))}
+               
+            </tbody>
+        </table>        
     }
 }
  
