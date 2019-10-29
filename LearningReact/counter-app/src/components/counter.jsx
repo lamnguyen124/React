5c9a3bff320fcc99d64dc703159ef6ca467fd58c
Lam <nguyen.h.lam12@gmail.com>
2019-10-29 10:50:45 -0500

    increment button works

--- LearningReact/counter-app/src/components/counter.jsx
+++ LearningReact/counter-app/src/components/counter.jsx
@@ -1,40 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: []
     };
 
-    styles = {
-        fontSize: 50,
-        fontWeight: 'bold'
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIcrement.bind(this);
+    // }
+
+    handleIcrement = () => {
+        this.setState({ count: this.state.count + 1 })
     };
+
+    renderTags() {
+        if (this.state.tags.length === 0)
+            return <p>There are no tags!</p>;
+
+
+
+        return <ul> {
+            this.state.tags.map(tag => <li key={tag}>
+                {tag}</li>)
+        } </ul>
+    }
 
     render() {
         return (
-            <React.Fragment>
-                <span className={
+            <React.Fragment>{
+                this.state.tags.length === 0 && "Please create a new tag!"
+            }
+                {
+                    this.renderTags()
+                }<span className={
                     this.getBadgeClasses()
                 }>
                     {
-                    this.formatCount()
-                }</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                        this.formatCount()
+                    } </span>
+                <button onClick={
+                    this.handleIcrement
+                }
+                    className="btn btn-secondary btn-sm">Increment
+                </button>
             </React.Fragment>
         );
     }
 
     getBadgeClasses() {
-        let classes = "badge m-2 badge-";
+        let classes = "badge m-2 badge";
         classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state;
+        const { count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
-
 }
 
 export default Counter;
